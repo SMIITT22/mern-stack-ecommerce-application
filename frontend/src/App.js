f7234883c30fe381b60b306db217b0fc3d00af73
SMIITT22 <prosmit2707@gmail.com>
2023-02-12 14:02:30 +0100

    Separate code to HomeScreen and ProductScreen from App.js file and react-router-dom has been added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,21 @@
-import data from './data';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
-function App () {
+function App() {
   return (
-    <div>
-      <header>
-        <a href="/">amazona</a>
-      </header>
-      <main>list products</main>
-      <main>
-        <h1>Featured Products</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="product-info">
-                <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to = "/">amazona</Link>
+        </header>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/product/:slug" element={<ProductScreen />} />
+        </Routes>
+        <main></main>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,9 @@
+import { useParams } from 'react-router-dom';
+
+function ProductScreen() {
+  const params = useParams();
+  const { slug } = params;
+  console.log({slug})
+  return (<div><h1>{slug}</h1></div>);
+}
+export default ProductScreen;
