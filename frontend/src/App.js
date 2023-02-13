c49dc78da35a84ea3d63a64ca632c5cc41d69db5
SMIITT22 <prosmit2707@gmail.com>
2023-02-13 11:38:22 +0100

    bootstrap has been added for designing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,32 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className='d-flex flex-column site-container'>
         <header>
-          <Link to="/">amazona</Link>
+          <Navbar bg="dark" variant="dark">
+            <LinkContainer to="/">
+              <Navbar.Brand>
+                <div style={{ padding: '1rem' }}>amazon</div>
+              </Navbar.Brand>
+            </LinkContainer>
+          </Navbar>
         </header>
-        <Routes>
-          <Route path="/" element={<HomeScreen />} />
-          <Route path="/product/:slug" element={<ProductScreen />} />
-        </Routes>
-        <main></main>
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/product/:slug" element={<ProductScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer><div className='text-center'>All rights reserved @2023</div></footer>
       </div>
     </BrowserRouter>
   );
