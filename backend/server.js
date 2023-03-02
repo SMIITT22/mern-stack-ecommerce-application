ef88a6092493069a82900ba4c05fe801b32d9423
SMIITT22 <prosmit2707@gmail.com>
2023-03-02 17:22:13 +0100

    connected to the mongoDB(mongoose and dotenv installed)

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,46 @@
-import express from 'express'; 
+import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('connected to db');
+  })
+  .catch(err => {
+    console.log(err.message);
+  });
 
 //01 api --> for homescreen data
 const app = express();
 app.get('/api/products', (req, res) => {
-    res.send(data.products);
-})
-
+  res.send(data.products);
+});
 
 //02 api --> for each product data
 app.get('/api/products/slug/:slug', (req, res) => {
-    const product = data.products.find(x => x.slug === req.params.slug);
-    if(product){
-        res.send(product)
-    }
-    else{
-        res.status(404).send({message : 'Product Not Found'})
-    }
-})
+  const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
 
 //03 api for cart logic (adding items into the cart)
 app.get('/api/products/:id', (req, res) => {
-    const product = data.products.find(x => x._id === req.params.id);
-    if(product){
-        res.send(product)
-    }
-    else{
-        res.status(404).send({message : 'Product Not Found'})
-    }
-})
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
 
 const port = process.env.PORT || 5000;
-app.listen(port, ()=>{
-    console.log(`server at http://localhost:${port}`)
-})
+app.listen(port, () => {
+  console.log(`server at http://localhost:${port}`);
+});
